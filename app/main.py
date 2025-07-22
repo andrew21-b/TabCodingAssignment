f664b2886deb97bdfa2ec7209b36ccafaa83ddb2
Andrew Boateng <andrew.21@outlook.com>
2025-07-22 22:26:59 +0100

    Added routes to API

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from app.controller.v1.routes import api_router
 from app.context.config import settings
 
 app = FastAPI(title=settings.PROJECT_NAME, version=settings.VERSION)
+
+app.include_router(api_router, prefix="/api/v1")
 
 @app.get("/")
 def root():
