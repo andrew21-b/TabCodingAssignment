d793a5c8e1939f002cbbba9f1b8038dbd89acec1
Andrew Boateng <andrew.21@outlook.com>
2025-07-22 17:41:12 +0100

    Updated config.py to include paths to data scources

--- app/context/config.py
+++ app/context/config.py
@@ -3,6 +3,8 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Tab Coding Assignment"
     VERSION: str = "1.0"
+    ACCOUNTS_DATA_PATH: str = "data/accounts.json"
+    TRANSACTIONS_DATA_PATH: str = "data/transactions.json"
 
 
 settings = Settings()
