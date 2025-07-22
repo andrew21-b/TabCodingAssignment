ba102a4147c98c33991a6a6c09e46c03cf9af7ae
Andrew Boateng <andrew.21@outlook.com>
2025-07-22 17:02:42 +0100

    Updated main.py to have a root endpoint

--- app/main.py
+++ app/main.py
@@ -2,3 +2,7 @@ from fastapi import FastAPI
 from app.context.config import settings
 
 app = FastAPI(title=settings.PROJECT_NAME, version=settings.VERSION)
+
+@app.get("/")
+def root():
+    return {"message": f"Welcome to {settings.PROJECT_NAME} API version {settings.VERSION}"}
